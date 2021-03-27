f8c9b0764813e2b85a04502a1d462ded7d777b9b
William Capozzoli <thinkocapo@gmail.com>
2021-03-27 18:36:50 -0500

    save

---
+++ pages/[id].js
@@ -0,0 +1,5 @@
+function DynamicRoot() {
+    return <div>DynamicRoot localhost:3000/[id]</div>
+  }
+  
+  export default DynamicRoot
---
+++ pages/posts/[id].js
@@ -0,0 +1,5 @@
+function Post() {
+    return <div>Posts dynamic route</div>
+  }
+  
+  export default Post
