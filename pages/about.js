67a3efa5334b7209121d40ccbb787ba15855e276
William Capozzoli <thinkocapo@gmail.com>
2021-03-28 16:06:20 -0500

    global styles, metadata

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,11 @@
+import Image from 'next/image'
 function About() {
-    return <div>About</div>
+    return <Image
+    src="/images/newsletter-photo-1.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Will Cap"
+  />
   }
   
   export default About
