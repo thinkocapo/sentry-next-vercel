709d415d76981db86f66857facaa81cd51597ac4
William Capozzoli <thinkocapo@gmail.com>
2021-03-28 21:06:48 -0500

    server-side rendered

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,19 @@
 import Image from 'next/image'
+
+/*
+Adding getServerSideProps causes the `npm run build` to output it as (Server) server-side renders at runtime (uses getServerSideProps)
+*/
+export async function getServerSideProps(context) {
+    return {
+      props: {
+        // props for your component
+      }
+    }
+  }
+
 function About() {
     return <Image
-    src="/images/newsletter-photo-1.jpg" // Route of the image file
+    src="/images/profile.jpg" // Route of the image file
     height={144} // Desired size with correct aspect ratio
     width={144} // Desired size with correct aspect ratio
     alt="Will Cap"
